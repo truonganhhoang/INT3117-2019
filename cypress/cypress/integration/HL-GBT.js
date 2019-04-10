57360ad4f657fb374dc06ac21954e6338599eb22
unknown <ian@cinnamon.is>
2019-04-10 15:12:37 +0700

    update

--- cypress/cypress/integration/HL-GBT.js
+++ cypress/cypress/integration/HL-GBT.js
@@ -1,4 +1,5 @@
 context('Giao bai tap', () => {
+  var time = 1000
 
   beforeEach( () => {
     cy.login({ url: 'https://hoclieu.sachmem.vn', user_name: 'vmhieu17@gmail.com', password: '12345678' })
@@ -9,7 +10,7 @@ context('Giao bai tap', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 6').click()
     cy.contains('Phiên bản trước').click()
-    cy.wait(2000);
+    cy.wait(time);
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).should('not.exist')
   })
@@ -17,13 +18,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_33 (row 51)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var chon_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select'
     cy.get(chon_lop_hoc).click()
     var first_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select > ng-dropdown-panel > div > div:nth-child(2) > div.ng-option.ng-option-marked'
@@ -37,13 +38,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_32 (row 50)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var chon_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select'
     cy.get(chon_lop_hoc).click()
     var first_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select > ng-dropdown-panel > div > div:nth-child(2) > div.ng-option.ng-option-marked'
@@ -57,13 +58,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_88 (row 143)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var chon_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select'
     cy.get(chon_lop_hoc).click()
     var first_lop_hoc = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(3) > ng-select > ng-dropdown-panel > div > div:nth-child(2) > div.ng-option.ng-option-marked'
@@ -77,13 +78,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_38 (row 57)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var thoi_gian_lam_bai = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(6) > input'
     cy.get(thoi_gian_lam_bai).should('be.empty')
   })
@@ -91,13 +92,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_37 (row 58)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var thoi_gian_lam_bai = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(6) > input'
     cy.get(thoi_gian_lam_bai).type('abc').should('not.have.value', 'abc')
   })
@@ -105,13 +106,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_36 (row 59)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var thoi_gian_lam_bai = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(6) > input'
     cy.get(thoi_gian_lam_bai).type('@@~').should('not.have.value', '@@~')
   })
@@ -119,13 +120,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_37 (row 60)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var thoi_gian_lam_bai = '#createAssignmentModal > div > div > form > div.modal-body > div:nth-child(6) > input'
     cy.get(thoi_gian_lam_bai).type('60').should('have.value', '60')
   })
@@ -133,13 +134,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_87 (row 142)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     var today = new Date()
     var dd = String(today.getDate()-1).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -156,13 +157,13 @@ context('Giao bai tap', () => {
   it('HL-GBT_31 (row 80)', () => {
     cy.visit('https://hoclieu.sachmem.vn')
     cy.contains('Lớp 7').click()
-    cy.wait(2000)
+    cy.wait(time)
     var first_book = 'body > app-root > div.content-bound > div > div > div > folder-tree > ul > li:nth-child(2) > div > div > a'
     cy.get(first_book).first().click()
-    cy.wait(2000)
+    cy.wait(time)
     var giao_bai_button = 'body > app-root > div.content-bound > div > div > div > question > div > div.main.main-closed > div > div > app-preview-list-question > div > div > div:nth-child(1) > div.pull-right'
     cy.get(giao_bai_button).click()
-    cy.wait(2000)
+    cy.wait(time)
     cy.contains('Tạo').click()
     cy.contains('Trường này không được để trống').should('exist')
   })
